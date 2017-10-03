4d65961d2286ba08302f699029f3f97d44ebb6c4
michalmuller <31619852+michalmuller@users.noreply.github.com>
2017-10-03 15:04:33 +0200

    delete not working

--- CentiSoft/DAL/ContactRepository.cs
+++ CentiSoft/DAL/ContactRepository.cs
@@ -133,5 +133,26 @@ namespace CentiSoft.DAL
             return contact;
         }
 
+        public void DeleteContact(int id)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=localhost;Database=PWEContact;Integrated Security=SSPI";
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "DELETE FROM Contact WHERE Id=" + id;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
